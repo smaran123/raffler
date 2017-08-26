a475cc9268b91bbb7f5ad2f8254c292bc686a2ca
bhagath <bhagath@getcustomexposure.com>
2017-08-27 00:11:38 +0530

    angular unknown provider not resolved

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Entry.create!(name: "Matz")
+Entry.create!(name: "DHH")
+Entry.create!(name: "Jose Valim")
+Entry.create!(name: "Avdi Grimm")
+Entry.create!(name: "Steve Klabnik")
+Entry.create!(name: "Aaron Patterson")
